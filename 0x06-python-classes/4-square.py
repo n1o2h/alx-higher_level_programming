97c55624bce2548d74e0febbeed709c57840db5a
n1o2h <aythmadnhylt@gmail.com>
2024-01-15 06:30:20 -0800

    task 4

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+class Square:
+    def __init__(self, size=0):
+        self.__size = size
+    @property
+    def size(self):
+        return self.__size
+    
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError('size most be an integer')
+        if value < 0:
+            raise ValueError('size most be >= 0')
+        self.__size = value
+
+    def area(self):
+        return self.__size ** 2
